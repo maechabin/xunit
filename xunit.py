199ee024b248a24e7e512559f1d602b5675a65cc
maechabin <mitoppo@gmail.com>
2018-06-03 13:48:20 +0900

    失敗時のテスト追加

--- xunit.py
+++ xunit.py
@@ -1,7 +1,9 @@
 class TestResult:
     def __init__(self):
+        # 0で初期化
         self.runCount = 0
     def testStarted(self):
+        # テスト実行ごとにインクリメント
         self.runCount = self.runCount + 1
     def summary(self):
         return '%d run, 0 failed' % self.runCount;
@@ -14,6 +16,7 @@ class TestCase:
     def tearDown(self):
         pass
     def run(self):
+        # TestResultのインスタンスを生成
         result = TestResult()
         result.testStarted()
         self.setUp()
@@ -40,12 +43,19 @@ class TestCaseTest(TestCase):
         test = WasRun('testMethod')
         test.run()
         assert('setUp testMethod tearDown ' == test.log)
-    # テスト結果を返すかテスト
+    # テスト結果を返すかテスト（成功時）
     def testResult(self):
         # WasRunのインスタンスを生成（Fixture）
         test = WasRun('testMethod')
         result = test.run()
         assert('1 run, 0 failed' == result.summary())
+    # テスト結果を返すかテスト（失敗時）
+    def testFailedResult(self):
+        # WasRunのインスタンスを生成（Fixture）
+        test = WasRun('testBrokenMethod')
+        result = test.run()
+        assert('1 run, 1 failed' == result.summary())
 
 TestCaseTest('testTemplateMethod').run()
 TestCaseTest('testResult').run()
+TestCaseTest('testFailedResult').run()
